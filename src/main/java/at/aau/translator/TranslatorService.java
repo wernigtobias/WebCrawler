2cee43371929237e85f95ad469798d052209f294
niwiunig <niwiunig@edu.aau.at>
2024-06-11 18:29:09 +0200

    Added setHttpClient Method to TranslatorService

--- src/main/java/at/aau/translator/TranslatorService.java
+++ src/main/java/at/aau/translator/TranslatorService.java
@@ -1,8 +1,11 @@
 package at.aau.translator;
 
+import okhttp3.OkHttpClient;
+
 import java.io.IOException;
 
 public interface TranslatorService {
     boolean validateLanguage(String language);
     String translate(String text, String targetLanguage) throws IOException, TranslatorServiceException;
+    public void setHttpClient(OkHttpClient httpClient);
 }
