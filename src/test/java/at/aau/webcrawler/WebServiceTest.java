02b666e60a9168755b48d7b7f77e2e344fe98b60
niwiunig <niwiunig@edu.aau.at>
2024-06-11 18:30:35 +0200

    Added Tests for WebServiceTest

---
+++ src/test/java/at/aau/webcrawler/WebServiceTest.java
@@ -0,0 +1,40 @@
+package at.aau.webcrawler;
+
+import at.aau.services.WebService;
+import at.aau.webcrawler.dto.Webpage;
+import at.aau.webcrawler.dto.WebpageImpl;
+import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WebServiceTest {
+
+    private Document documentMock;
+    private Connection connectionMock;
+
+    @Test
+    void testGetDocument() {
+        try (MockedStatic<Jsoup> mockedStatic = Mockito.mockStatic(Jsoup.class)) {
+            initForSuccessTest();
+            assertEquals(documentMock, Jsoup.connect("url").get());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    private void initForSuccessTest() throws IOException {
+        documentMock = Mockito.mock(Document.class);
+        connectionMock = Mockito.mock(Connection.class);
+        Mockito.when(Jsoup.connect(Mockito.anyString())).thenReturn(connectionMock);
+        Mockito.when(connectionMock.get()).thenReturn(documentMock);
+    }
+}
