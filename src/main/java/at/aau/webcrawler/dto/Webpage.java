4061f1d5a56d02dd814ffca6e1faf45a2b651e50
niwiunig <niwiunig@edu.aau.at>
2024-06-11 08:35:40 +0200

    Added interface for Webpage

--- src/main/java/at/aau/webcrawler/dto/Webpage.java
+++ src/main/java/at/aau/webcrawler/dto/Webpage.java
@@ -1,24 +1,10 @@
 package at.aau.webcrawler.dto;
 
-import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
-public class Webpage {
-  private final Document document;
-
-  public Webpage(Document document) {
-    this.document = document;
-  }
-
-  public Elements getHeadings() {
-    return document.select("h1,h2,h3,h4,h5,h6");
-  }
-
-  public Set<String> getLinks() {
-    Elements linkElements = document.select("a");
-    return linkElements.stream().map(element -> element.attr("href")).collect(Collectors.toSet());
-  }
+public interface Webpage {
+    Elements getHeadings();
+    Set<String> getLinks();
 }
---
+++ src/main/java/at/aau/webcrawler/dto/WebpageImpl.java
@@ -0,0 +1,24 @@
+package at.aau.webcrawler.dto;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class WebpageImpl implements Webpage {
+  private final Document document;
+
+  public WebpageImpl(Document document) {
+    this.document = document;
+  }
+
+  public Elements getHeadings() {
+    return document.select("h1,h2,h3,h4,h5,h6");
+  }
+
+  public Set<String> getLinks() {
+    Elements linkElements = document.select("a");
+    return linkElements.stream().map(element -> element.attr("href")).collect(Collectors.toSet());
+  }
+}
