3bbc2d9ff28ebb52d1713b3ac5e09187f874a2d7
niwiunig <niwiunig@edu.aau.at>
2024-06-11 08:36:47 +0200

    Added hardcoded values into App.java for testing

--- src/main/java/at/aau/app/App.java
+++ src/main/java/at/aau/app/App.java
@@ -12,16 +12,20 @@ public class App {
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        String[] urls = readURLs();
-        maxDepth = readDepth();
-        String[] domains = readDomains();
+        //String[] urls = readURLs();
+        String[] urls = new String[]{"https://ecockpit.at", "https://softwaregutachten.at/"};
+        //maxDepth = readDepth();
+        maxDepth = 3;
+        //String[] domains = readDomains();
+        String[] domains = new String[]{"ecockpit.at", "softwaregutachten.at", "de.wikipedia.org"};
+
         scanner.close();
 
         initializeCrawlerAndRun(urls, domains);
     }
 
     private static void initializeCrawlerAndRun(String[] urls, String[] domains) {
-        WebCrawlerExecutor.initializeThreadPoolWithThreadCount(20);
+        WebCrawlerExecutor.initializeThreadPoolWithThreadCount(300);
         WebCrawlerImpl crawler = new WebCrawlerImpl();
         crawler.run(urls, domains);
     }
