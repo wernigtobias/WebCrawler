d22ec5d4d1fee84bc9274e49b504312513b8528e
niwiunig <niwiunig@edu.aau.at>
2024-06-11 18:27:42 +0200

    Changed Error-Messages for loadWebpage

--- src/main/java/at/aau/services/WebService.java
+++ src/main/java/at/aau/services/WebService.java
@@ -10,17 +10,15 @@ import java.io.IOException;
 
 public class WebService {
 
-  public static Webpage loadWebpage(String url) {
-    try {
-      Document document = Jsoup.connect(url).get();
-      return new WebpageImpl(document);
-    } catch (HttpStatusException e) {
-      if (e.getStatusCode() == 404) {
+    public static Webpage loadWebpage(String url) {
+        try {
+            Document document = Jsoup.connect(url).get();
+            return new WebpageImpl(document);
+        } catch (HttpStatusException e) {
+            System.out.println("[ERROR] Failed to load Webpage " + url + " because of StatusCode: " + e.getStatusCode());
+        } catch (IOException ie) {
+            System.out.println("[ERROR] Failed connecting: " + ie.getMessage());
+        }
         return null;
-      }
-    } catch (IOException ie) {
-      System.out.println("[ERROR] Failed connecting: " + ie.getMessage());
     }
-    return null;
-  }
 }
